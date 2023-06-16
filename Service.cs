5627cd204b4af0e5b2f6e11fc690660d74920250
Fryctik <83564951+Fryctik@users.noreply.github.com>
2023-06-16 22:40:25 +0400

    Add files via upload

---
+++ Service.cs
@@ -0,0 +1,18 @@
+﻿using avaloni.Models;
+
+namespace avaloni;
+
+public class Service
+{
+    private static ResourceContext? _db;
+
+    public static ResourceContext GetDbContext()
+    {
+        if (_db == null)
+        {
+            _db = new ResourceContext();
+        }
+
+        return _db;
+    }
+}
